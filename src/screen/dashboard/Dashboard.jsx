1f521d6e86d75b1dd1d147af1672f9b3d74d7ff9
UnziShaikh <unzishaikh137@gmail.com>
2024-10-08 10:44:34 +0500

    Update Dashboard.jsx

--- src/screen/dashboard/Dashboard.jsx
+++ src/screen/dashboard/Dashboard.jsx
@@ -21,29 +21,29 @@ import MailIcon from "@mui/icons-material/Mail";
 import { user_is_signin } from "../../config/FirebaseMethods";
 import { Outlet, useNavigate } from "react-router-dom";
 
-// style
+style
 import "../../style/dashboard.scss";
 
 const drawerWidth = 240;
 
-// const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
-//   ({ theme, open }) => ({
-//     flexGrow: 1,
-//     padding: theme.spacing(3),
-//     transition: theme.transitions.create("margin", {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-//     marginLeft: `-${drawerWidth}px`,
-//     ...(open && {
-//       transition: theme.transitions.create("margin", {
-//         easing: theme.transitions.easing.easeOut,
-//         duration: theme.transitions.duration.enteringScreen,
-//       }),
-//       marginLeft: 0,
-//     }),
-//   })
-// );
+ const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+   ({ theme, open }) => ({
+     flexGrow: 1,
+     padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+       easing: theme.transitions.easing.sharp,
+       duration: theme.transitions.duration.leavingScreen,
+     }),
+     marginLeft: `-${drawerWidth}px`,
+     ...(open && {
+       transition: theme.transitions.create("margin", {
+         easing: theme.transitions.easing.easeOut,
+         duration: theme.transitions.duration.enteringScreen,
+       }),
+       marginLeft: 0,
+     }),
+   })
+ );
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "open",
